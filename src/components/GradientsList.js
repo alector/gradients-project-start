17cb517a2a7aebf308af0d2c7012a22d9d5645d4
alector <panagiotispp@gmail.com>
2021-04-12 18:55:02 +0200

    logique state update

--- src/components/GradientsList.js
+++ src/components/GradientsList.js
@@ -1,19 +1,16 @@
 import Gradient from "./Gradient"
-const GradientsList = () => {
+const GradientsList = ({ gradients, selectedTag }) => {
   return (
     <ul className="row list-unstyled">
-      <Gradient colorStart="rgb(189, 195, 199)" colorEnd="rgb(44, 62, 80)" name="Grade Grey" />
-      <Gradient colorStart="rgb(31, 64, 55)" colorEnd="rgb(153, 242, 200)" name="Harvey" />
-      <Gradient colorStart="rgb(0, 242, 96)" colorEnd="rgb(5, 117, 230)" name="Rainbow Blue" />
-      <Gradient colorStart="rgb(168, 192, 255)" colorEnd="rgb(63, 43, 150)" name="Slight Ocean View" />
+      {gradients.map(gradient => {
+        const TagsArray = gradient.tags
 
-      <Gradient colorStart="rgb(168, 192, 255)" colorEnd="rgb(63, 43, 150)" name="Slight Ocean View" />
+        if (TagsArray.includes(selectedTag)) {
+          return <Gradient colorStart={gradient.start} colorEnd={gradient.start} name={gradient.name} selectedTag={selectedTag} />
+        }
 
-      <Gradient colorStart="rgb(168, 192, 255)" colorEnd="rgb(63, 43, 150)" name="Slight Ocean View" />
-
-      <Gradient colorStart="rgb(168, 192, 255)" colorEnd="rgb(63, 43, 150)" name="Slight Ocean View" />
-
-      <Gradient colorStart="rgb(168, 192, 255)" colorEnd="rgb(63, 43, 150)" name="Slight Ocean View" />
+        return null
+      })}
     </ul>
   )
 }
