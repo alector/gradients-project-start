4ecb811d63137bc573ab0c8d63841d8d02d19738
alector <panagiotispp@gmail.com>
2021-04-12 16:54:57 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import GradientsList from "./components/GradientsList"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 function App() {
   return (
     <div>
       <Header />
-      {/* <h1 className="text-center my-4">Alyra Gradients</h1>
+      <h1 className="text-center my-4">Alyra Gradients</h1>
       <main className="container">
         <GradientsList />
-      </main> */}
+      </main>
+      <Footer />
     </div>
   )
 }
